a66547adf008426668a5b8fab923256a541127be
Richard Kojedzinszky <krichy@tvnetwork.hu>
2014-02-14 00:06:30 +0100

    ZFS/GFS handling fixes

--- sys/cddl/compat/opensolaris/kern/opensolaris_lookup.c
+++ sys/cddl/compat/opensolaris/kern/opensolaris_lookup.c
@@ -81,6 +81,10 @@ traverse(vnode_t **cvpp, int lktype)
 	 * progress on this vnode.
 	 */
 
+	error = vn_lock(cvp, lktype);
+	if (error)
+		return (error);
+
 	for (;;) {
 		/*
 		 * Reached the end of the mount chain?
@@ -89,13 +93,7 @@ traverse(vnode_t **cvpp, int lktype)
 		if (vfsp == NULL)
 			break;
 		error = vfs_busy(vfsp, 0);
-		/*
-		 * tvp is NULL for *cvpp vnode, which we can't unlock.
-		 */
-		if (tvp != NULL)
-			vput(cvp);
-		else
-			vrele(cvp);
+		VOP_UNLOCK(cvp, 0);
 		if (error)
 			return (error);
 
@@ -107,6 +105,9 @@ traverse(vnode_t **cvpp, int lktype)
 		vfs_unbusy(vfsp);
 		if (error != 0)
 			return (error);
+
+		VN_RELE(cvp);
+
 		cvp = tvp;
 	}
 
